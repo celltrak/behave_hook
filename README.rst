422247884f7d472742a34df73f4634ffc6bc0463
Alan Rizzo <arizzo@celltrak.com>
2022-01-28 15:19:40 -0300

    hook version 0.0.1

---
+++ README.rst
@@ -0,0 +1,5 @@
+# Experimental 
+Tested for **pyenv**
+
+## Behave pre-commit hook
+If there are unused steps this hook will shows them
--- behave_hook/__init__.py
+++ behave_hook/__init__.py
@@ -1,35 +1,57 @@
+import os
+import sys
 import tempfile
+from glob import glob
 from pathlib import Path
 from typing import List
 
 from behave.__main__ import main as behave_main
-from behave.configuration import Configuration
-
-__version__ = "0.1.0"
 
 
 def run() -> bool:
+    path_finder()
+    execution_result = execute_behave()
+    return find_unused_steps(execution_result)
+
+
+def execute_behave() -> List[str]:
     tmp = tempfile.NamedTemporaryFile()
-    Configuration.defaults["show_timings"] = False
-    path = Path.cwd().joinpath("features").as_posix()
-    Configuration.defaults["dry_run"] = True
-    behave_main([path, "--format", "steps.usage", "--outfile", tmp.name])
+    behave_path = Path.cwd().joinpath("features").as_posix()
+    behave_main(
+        [
+            behave_path,
+            "--no-summary",
+            "--format",
+            "steps.usage",
+            "--dry-run",
+            "--outfile",
+            tmp.name,
+        ]
+    )
 
     with open(tmp.name) as openfile:
         lines = openfile.readlines()
 
-    return find_unused_steps(lines)
+    return lines
 
 
 def find_unused_steps(lines: List[str]) -> bool:
-    result = []
-    start_saving = False
+    exists_unused_steps = False
 
     for line in lines:
         if line.startswith("UNUSED STEP DEFINITIONS"):
-            start_saving = True
-        if start_saving:
-            result.append(line)
+            exists_unused_steps = True
+        if exists_unused_steps:
+            print(line)
 
-    print(result)
-    return bool(result)
+    return exists_unused_steps
+
+
+def path_finder() -> None:
+    """Add app virtual env to Path"""
+    paths = sys.path
+    virtual_env = os.environ.get("PYENV_VIRTUAL_ENV")
+    for venv_path in glob(f"{virtual_env}/lib/python*/site-packages"):
+        if venv_path not in paths:
+            paths.insert(0, venv_path)
+    sys.path = paths

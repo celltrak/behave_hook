a940a48369f8b66bacc8b7ef9f38563d23af3a5b
Alan Rizzo <arizzo@celltrak.com>
2022-01-27 22:31:12 -0300

    first commit

--- behave_hook/__init__.py
+++ behave_hook/__init__.py
@@ -1,1 +1,35 @@
-__version__ = '0.1.0'
+import tempfile
+from pathlib import Path
+from typing import List
+
+from behave.__main__ import main as behave_main
+from behave.configuration import Configuration
+
+__version__ = "0.1.0"
+
+
+def run() -> bool:
+    tmp = tempfile.NamedTemporaryFile()
+    Configuration.defaults["show_timings"] = False
+    path = Path.cwd().joinpath("features").as_posix()
+    Configuration.defaults["dry_run"] = True
+    behave_main([path, "--format", "steps.usage", "--outfile", tmp.name])
+
+    with open(tmp.name) as openfile:
+        lines = openfile.readlines()
+
+    return find_unused_steps(lines)
+
+
+def find_unused_steps(lines: List[str]) -> bool:
+    result = []
+    start_saving = False
+
+    for line in lines:
+        if line.startswith("UNUSED STEP DEFINITIONS"):
+            start_saving = True
+        if start_saving:
+            result.append(line)
+
+    print(result)
+    return bool(result)
---
+++ behave_hook/__main__.py
@@ -0,0 +1,5 @@
+from . import run
+import sys
+
+if __name__ == "__main__":
+    sys.exit(run())
